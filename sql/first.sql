fb8237b2dbe78cbbb6463e1b1cb3dc4f431b2ee6
yusakazuki <yusakazuki0323@gmail.com>
2016-01-15 08:59:56 +0900

    sqlテーブル、カラム修正

--- sql/first.sql
+++ sql/first.sql
@@ -4,20 +4,24 @@ use opinion_box;
 
 ---質問テーブル---
 CREATE TABLE questions (
-id INTEGER NOT NULL PRIMARY KEY AUTO_INCREMENT COMMENT'アンケートID',
-question_name vachar(30) NOT NULL COMMENT 'アンケート名'
-question_content varchar(50) NOT NULL COMMENT 'アンケート内容'
-choice1 text NOT NULL COMMENT 'アンケート選択肢1'
-choice2 text NOT NULL COMMENT 'アンケート選択肢2'
-choice3 text NOT NULL COMMENT 'アンケート選択肢3'
-choice4 text NOT NULL COMMENT 'アンケート選択肢4'
-quetion_status INTEGER NOTNULL DEFAULT 0 COMMENT 'アンケート属性(0:投票中 1:集計)'
+id INTEGER NOT NULL PRIMARY KEY AUTO_INCREMENT COMMENT,
+question_name vachar(30) NOT NULL COMMENT 'アンケート名',
+question_content text NOT NULL COMMENT 'アンケート内容',
+quetion_status INTEGER NOTNULL DEFAULT 0 COMMENT 'アンケート属性(0:投票中 1:集計)',
+);
+
+--選択肢テーブルーー
+CREATE TABLE choices (
+id INTEGER NOT NULL PRIMARY KEY AUTO_INCREMENT,
+question_id INTEGER NOT NULL COMMENT 'アンケートID',
+choice vachar(30) NOT NULL COMMMENT '選択肢'
 );
 
 ---回答テーブル---
-CREATE TABLE answer (
-id INTEGER NOT NULL PRIMARY KEY AUTO_INCREMENT 
-answer_choice INTEGER NOT NULL COMMENT '回答' 
+CREATE TABLE answers (
+id INTEGER NOT NULL PRIMARY KEY AUTO_INCREMENT,
+question_id INTEGER NOT NULL COMMENT'アンケートID',
+answer INTEGER NOT NULL COMMENT '回答',
 );
 
 
