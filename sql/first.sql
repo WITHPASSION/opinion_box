1beadd0b394f48440ff4ece678e46305c96beac7
yusakazuki <yusakazuki0323@gmail.com>
2016-01-15 09:04:23 +0900

    誤字修正

--- sql/first.sql
+++ sql/first.sql
@@ -5,23 +5,23 @@ use opinion_box;
 ---質問テーブル---
 CREATE TABLE questions (
 id INTEGER NOT NULL PRIMARY KEY AUTO_INCREMENT COMMENT,
-question_name vachar(30) NOT NULL COMMENT 'アンケート名',
+question_name varchar(30) NOT NULL COMMENT 'アンケート名',
 question_content text NOT NULL COMMENT 'アンケート内容',
-quetion_status INTEGER NOTNULL DEFAULT 0 COMMENT 'アンケート属性(0:投票中 1:集計)',
+quetion_status INTEGER NOTNULL DEFAULT 0 COMMENT 'アンケート属性(0:投票中 1:集計)'
 );
 
 --選択肢テーブルーー
 CREATE TABLE choices (
 id INTEGER NOT NULL PRIMARY KEY AUTO_INCREMENT,
 question_id INTEGER NOT NULL COMMENT 'アンケートID',
-choice vachar(30) NOT NULL COMMMENT '選択肢'
+choice varchar(30) NOT NULL COMMMENT '選択肢'
 );
 
 ---回答テーブル---
 CREATE TABLE answers (
 id INTEGER NOT NULL PRIMARY KEY AUTO_INCREMENT,
 question_id INTEGER NOT NULL COMMENT'アンケートID',
-answer INTEGER NOT NULL COMMENT '回答',
+answer INTEGER NOT NULL COMMENT '回答'
 );
 
 
