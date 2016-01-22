54356fcff093663e4fd079ffc2f4a49d172b0b0c
yusakazuki <k.yusa@senmonka-kensaku.com>
2016-01-22 17:25:41 +0900

    エラー部分を排除する

--- sql/first.sql
+++ sql/first.sql
@@ -2,24 +2,24 @@ CREATE DATABASE opinion_box;
 
 use opinion_box;
 
----質問テーブル---
+--　質問テーブル --
 CREATE TABLE questions (
-id INTEGER NOT NULL PRIMARY KEY AUTO_INCREMENT COMMENT,
+id INTEGER NOT NULL PRIMARY KEY AUTO_INCREMENT,
 question_name varchar(30) NOT NULL COMMENT 'アンケート名',
 question_content text NOT NULL COMMENT 'アンケート内容',	
-quetion_status INTEGER NOT　NULL DEFAULT 0 COMMENT 'アンケート属性(0:投票中 1:集計)'
+quetion_status INTEGER NOT NULL DEFAULT 0 COMMENT 'アンケート属性(0:投票中 1:集計)'
 );
 
---選択肢テーブルーー
+-- 選択肢テーブル --
 CREATE TABLE choices (
 id INTEGER NOT NULL PRIMARY KEY AUTO_INCREMENT,
 question_id INTEGER NOT NULL COMMENT 'アンケートID',
 choice_number INTEGER NOT NULL COMMENT '選択肢番号',
-choice varchar(30) NOT NULL COMMMENT '選択肢',
+choice varchar(30) NOT NULL COMMENT '選択肢',
 UNIQUE(question_id,choice_number)
 );
 
----回答テーブル---
+-- 回答テーブル --
 CREATE TABLE answers (
 id INTEGER NOT NULL PRIMARY KEY AUTO_INCREMENT,
 question_id INTEGER NOT NULL COMMENT'アンケートID',
