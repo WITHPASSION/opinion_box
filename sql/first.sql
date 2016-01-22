a5af798c348c2a84dd106fcbd7332fe9fe4ec0b8
yusakazuki <yusakazuki0323@gmail.com>
2016-01-22 10:01:35 +0900

    選択肢の順番ソートさせるため

--- sql/first.sql
+++ sql/first.sql
@@ -6,22 +6,24 @@ use opinion_box;
 CREATE TABLE questions (
 id INTEGER NOT NULL PRIMARY KEY AUTO_INCREMENT COMMENT,
 question_name varchar(30) NOT NULL COMMENT 'アンケート名',
-question_content text NOT NULL COMMENT 'アンケート内容',
-quetion_status INTEGER NOTNULL DEFAULT 0 COMMENT 'アンケート属性(0:投票中 1:集計)'
+question_content text NOT NULL COMMENT 'アンケート内容',	
+quetion_status INTEGER NOT　NULL DEFAULT 0 COMMENT 'アンケート属性(0:投票中 1:集計)'
 );
 
 --選択肢テーブルーー
 CREATE TABLE choices (
 id INTEGER NOT NULL PRIMARY KEY AUTO_INCREMENT,
 question_id INTEGER NOT NULL COMMENT 'アンケートID',
-choice varchar(30) NOT NULL COMMMENT '選択肢'
+choice_number INTEGER NOT NULL COMMENT '選択肢番号',
+choice varchar(30) NOT NULL COMMMENT '選択肢',
+UNIQUE(question_id,choice_number)
 );
 
 ---回答テーブル---
 CREATE TABLE answers (
 id INTEGER NOT NULL PRIMARY KEY AUTO_INCREMENT,
 question_id INTEGER NOT NULL COMMENT'アンケートID',
-answer INTEGER NOT NULL COMMENT '回答'
+choice_id INTEGER NOT NULL COMMENT '回答'
 );
 
 
