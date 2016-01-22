2ef1ea7e59a39c168f8636fbc7b42c90f134ecbc
yusakazuki <yusakazuki0323@gmail.com>
2016-01-22 11:39:22 +0900

    デフォルトをtopに変えたため

---
+++ application/views/top.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<?php
+	defined('BASEPATH') OR exit('No direct script access allowed');
+?>
+<html lang = "ja">
+<head>
+<meta charset = "utf-8">
+     <title>topページ</title>
+</head>
+<body>
+
+
+
+
+</body>
+</html>
