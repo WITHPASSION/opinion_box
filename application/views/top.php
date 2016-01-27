dbab460b73718ecc6605d1a79b195b704a89b8d2
yusakazuki <k.yusa@senmonka-kensaku.com>
2016-01-27 12:57:48 +0900

    top画面の表示の枠組みを作る

--- application/views/top.php
+++ application/views/top.php
@@ -4,13 +4,44 @@
 ?>
 <html lang = "ja">
 <head>
-<meta charset = "utf-8">
+	<meta charset = "utf-8">
+	<link href="/assets/css/bootstrap.min.css" rel="stylesheet">
      <title>topページ</title>
 </head>
 <body>
-
-
-
+	<div class = "container-fluid">
+	<div class = "col-xs-6 col-xs-offset-3"> 
+	<table class = "table table-bordered table-hover">
+		<thead>
+			<tr>
+				<th class = "text-center">アンケート名</th>
+			</tr>
+		</thead>
+		<tbody>
+		<tr>
+			<td class = "text-center">sample1</td>
+		</tr>
+		<tr>
+			<td class = "text-center" >sample2</td>
+		</tr>
+		<tr>
+			<td class = "text-center">sample3</td>
+		</tr>
+		<tr>
+			<td class = "text-center">sample4</td>
+		</tr>
+	</table>
+	<tr>
+		<div class = "text-right">
+		<td>
+			<input type = "button" class = "btn-primary"value =  "アンケート作成">
+		</td>
+		</div>
+	</tr>
+	</tbody>
+	</div>　
+	</div>
+	</div>
 
 </body>
 </html>
