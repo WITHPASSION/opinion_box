a535fd28d9c70238db0fced5d342aaf276bdd102
yusakazuki <k.yusa@senmonka-kensaku.com>
2016-01-29 14:53:46 +0900

    新規質問作成画面のUI変更のため

--- application/views/new_question.php
+++ application/views/new_question.php
@@ -7,27 +7,44 @@
 </head>
 <body>
 
-<form action = "" method = "post">
-<div class = "container-fluid"
-<div class = "col-xs-6 col-xs-offset-3">
-	<label for = "name"> アンケート名</label>
+<form class = "form-horizontal" role = "form">
+<div class = "col-xs-offset-3">
+	<h3>アンケート作成画面</h3>
 	<div class = "form-group">
-	<input type = "text"  name = "name" size = "30" placeholder = "アンケート名">	
+	<label for = "name" class = "col-sm-2 control-label"> アンケート名</label>
+	<div class = "col-xs-4">
+	<input type = "text" class = "form-control"name = "name"  placeholder = "アンケート名">	
 	</div>
-	<label for = "content">内容</label>
+	</div>
 	<div class = "form-group">
-	<textarea name = "content" cols = "30" rows = "4" placeholder = "アンケート内容"></textarea>
+	<label for = "content" class = "col-sm-2 control-label">内容</label>
+	<div class = "col-xs-4">
+	<textarea class = "form-control" rows = "4" placeholder = "アンケート内容"></textarea>
 	</div>
-	<label for = "choice1">選択肢1</label>
+	</div>
 	<div class = "form-group">
-	<input type = "text" name = "choice1" size = "30" placeholder = "選択肢１">
+	<label for = "choice1" class = "col-sm-2 control-label">選択肢1</label>
+	<div class = "col-xs-3">
+	<input type = "text" class = "form-control"  name = "choice1" placeholder = "選択肢１">
 	</div>
-	<label for = "choice2">選択肢２</label>
+	</div>
 	<div class = "form-group">
-	<input type = "text" name = "choice2" size = "30" placeholder = "選択肢２">
+	<label for = "choice2" class = "col-sm-2 control-label">選択肢２</label>
+	<div class = "col-xs-3">
+	<input type = "text" class = "form-control" name = "choice2"  placeholder = "選択肢２">
 	</div>
+	</div>
+	</div>
+	<div class ="form-group">
+	<div class = "col-sm-offset-6 col-xs-10">
+	<input type = "button" class = "btn-primary" value = "選択肢追加">
+	</div>
+	</div>
+	<div class = "col-sm-offset-6">
+	<input type = "button" class = "btn-primary" value = "確認">
+	<input type =  "button" class = "btn-primary" value = "戻る">
 	</div>
 				   
-	</form>
+</form>
 </body>　
 </html>
