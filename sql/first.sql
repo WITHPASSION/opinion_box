2d08efdbc79ed969b8134d963a99214a9158be14
yusakazuki <k.yusa@senmonka-kensaku.com>
2016-02-01 17:10:01 +0900

    カラム名がDB設計と違うため変更

--- sql/first.sql
+++ sql/first.sql
@@ -7,7 +7,7 @@ CREATE TABLE questions (
 id INTEGER NOT NULL PRIMARY KEY AUTO_INCREMENT,
 question_name varchar(30) NOT NULL COMMENT 'アンケート名',
 question_content text NOT NULL COMMENT 'アンケート内容',	
-quetion_status INTEGER NOT NULL DEFAULT 0 COMMENT 'アンケート属性(0:投票中 1:集計)'
+question_status INTEGER NOT NULL DEFAULT 0 COMMENT 'アンケート属性(0:投票中 1:集計)'
 );
 
 -- 選択肢テーブル --
