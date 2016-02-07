b4f0c023c2cc189eb3b2952221894a8dd822905d
yusakazuki <k.yusa@senmonka-kensaku.com>
2016-02-07 18:26:23 +0900

    アンケート一覧画面に出すテストデータ追加

--- sql/test_date.sql
+++ sql/test_date.sql
@@ -1,7 +1,10 @@
 INSERT INTO questions
 (question_name , question_content , question_status)
 VALUES
-('テスト' , 'テストのため' , '1');
+('テスト' , 'テストのため' , '1'),
+('テスト2' , 'test' , '1' ),
+('hoge' , 'hogehoge' , '1' );
+
 
 INSERT INTO choices
 (question_id , choice_number , choice)
@@ -9,9 +12,19 @@ VALUES
 ('1' , '1' , 'テスト1'),
 ('1' , '2' , 'テスト2'),
 ('1' , '3' , 'テスト3'),
-('1' , '4' , 'テスト4');
+('1' , '4' , 'テスト4'),
+('2' , '1' , 'テスト1'),
+('2' , '2' , 'テスト2'),
+('2' , '3' , 'テスト3'),
+('2' , '4' , 'テスト4'),
+('3' , '1' , 'テスト1'),
+('3' , '2' , 'テスト2'),
+('3' , '3' , 'テスト3'),
+('3' , '4' , 'テスト4');
 
 INSERT INTO answers
 (question_id , choice_id)
 VALUES
-('1' , 2);
+('1' , '2'),
+('2' , '1'),
+('3' , '2');
