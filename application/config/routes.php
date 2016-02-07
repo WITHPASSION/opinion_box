f287cac6d0907e3f43b33c48c8487f37c0905afe
yusakazuki <k.yusa@senmonka-kensaku.com>
2016-02-07 16:40:16 +0900

    アンケート回答画面のURLルーティング設定

--- application/config/routes.php
+++ application/config/routes.php
@@ -50,6 +50,6 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 |		my-controller/my-method	-> my_controller/my_method
 */
 $route['default_controller'] = 'top';
-$route['choice'] = 'choice/$id';
+$route['choice/(:num)'] = 'choice/index/$1';
 $route['404_override'] = '';
 $route['translate_uri_dashes'] = FALSE;
