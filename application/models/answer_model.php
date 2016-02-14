159f8a217be76200113139d3e70e6a1387b13c1a
yusakazuki <k.yusa@senmonka-kensaku.com>
2016-02-14 17:12:54 +0900

    ファイル名変更

--- application/models/answer_model.php
+++ application/models/answer_model.php
@@ -1,25 +1,26 @@
 <?php
-class Answers extends CI_Model 
+class Answers_model extends CI_Model 
 {
 	function __construct()
 	{
 		parent::__construct();
 	}
 
-	function get_questionsAll()
+	function get_questions_all()
 	{
-			$this->load->database();
-			$query = $this->db->get('answer');
-			return $query->result();1
-}
+		$this->load->database();
+		$query = $this->db->get('answers');
+		return $query->result();
+	}
 	function insert_auestions()
 	{
-			$this->load->database();
-			$data = array(
-				'question_id' => $_POST['quetion_id'];
-				'choice_id' => $_POST['choice_id'];
-				);
-			$this->db->insert('answer' , $data);
+		$this->load->database();
+		$data = array(
+			'question_id' => $_POST['quetion_id'];
+			'choice_id' => $_POST['choice_id'];
+			);
+		$this->db->insert('answers' , $data);
+		return $data->result();
 	}
 }
 ?>
