2c80927cdbc1d703723f325df318456c00049171
yusakazuki <k.yusa@senmonka-kensaku.com>
2016-02-14 17:11:46 +0900

    ファイル名変更のため

--- application/config/routes.php
+++ application/config/routes.php
@@ -49,7 +49,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | Examples:	my-controller/index	-> my_controller/index
 |		my-controller/my-method	-> my_controller/my_method
 */
-$route['default_controller'] = 'top';
-$route['choice/(:num)'] = 'choice/index/$1';
+$route['default_controller'] = 'top_controller';
+$route['answer/(:num)'] = 'answer_controller/index/$1';
 $route['404_override'] = '';
 $route['translate_uri_dashes'] = FALSE;
