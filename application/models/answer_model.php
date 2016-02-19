f450ea9b9653b8e270b60ed0d36df5f79409a4a2
yusakazuki <k.yusa@senmonka-kensaku.com>
2016-02-19 12:48:56 +0900

    アンケート集計モデル

--- application/models/answer_model.php
+++ application/models/answer_model.php
@@ -12,11 +12,23 @@ class Answer_model extends CI_Model
 		$query = $this->db->get('answers');
 		return $query->result();
 	}
+
 	function insert_answer($answer)
 	{
 		$this->load->database();
 		$this->db->insert('answers' , $answer);
-		var_dump($answer);
 	}
+
+	function total($id)
+	{
+		$this->load->database();
+		$this->db->select('choice, COUNT(answers.id) as count');
+		$this->db->join('answers', 'choices.id=answers.choice_id', 'left');
+		$this->db->where('choices.question_id', $id);
+		$this->db->group_by('choice_id');
+		$query = $this->db->get('choices');
+		return $query->result();
+
+	}
+
 }
-?>
